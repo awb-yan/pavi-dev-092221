175993a7f375200909552de8b6059268c8196e15
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-09-22 17:40:11 +0800

    testing

--- awb_subscriber_bill_automation/models/subscription.py
+++ awb_subscriber_bill_automation/models/subscription.py
@@ -105,7 +105,7 @@ class SaleSubscription(models.Model):
                 except:
                     _logger.error(f'!!! Failed Temporary Discon - Subscription code {self.record.code}')
 
-            self.env['sale.subscription'].provision_and_activation(self.record, main_plan, last_subscription, ctp)
+            # self.env['sale.subscription'].provision_and_activation(self.record, main_plan, last_subscription, ctp)
 
             # Helper to update Odoo Opportunity
             # self._update_account(main_plan, self.record, sf_update_type, max_fail_retries)            
