f1b7f8d61afb90b71efef58d8166226ea69cda5b
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-10-07 18:27:24 +0800

    Config Settings

--- awb_subscriber_bill_automation/models/models.py
+++ awb_subscriber_bill_automation/models/models.py
@@ -29,78 +29,25 @@ class SubscriberBillAutomationModel(models.TransientModel):
             'context': "{'create': False}"
         }
 
-    # @api.model
-    # def set_values(self):
-    #     res = super(SubscriberBillAutomationModel, self).set_values()
-    #     self.env['ir.config_parameter'].set_param('odoo_salesforce.sf_sandbox', self.prepaid_phy_discon)
+    @api.model
+    def set_values(self):
+        res = super(SubscriberBillAutomationModel, self).set_values()
+        self.env['ir.config_parameter'].set_param('prepaid_physical_discon', self.prepaid_discon_days)
+        self.env['ir.config_parameter'].set_param('postpaid_physical_discon', self.postpaid_discon_days)
 
-    #     return res
+        return res
 
-    # @api.model
-    # def get_values(self):
-    #     res = super(SubscriberBillAutomationModel, self).get_values()
-    #     IrConfigParameter = self.env['ir.config_parameter'].sudo()
-    #     sandbox = IrConfigParameter.get_param('odoo_salesforce.sf_sandbox')
+    @api.model
+    def get_values(self):
+        res = super(SubscriberBillAutomationModel, self).get_values()
+        IrConfigParameter = self.env['ir.config_parameter'].sudo()
+        prepaid_days = IrConfigParameter.get_param('prepaid_physical_discon')
+        postpaid_days = IrConfigParameter.get_param('postpaid_physical_discon')
 
-    #     res.update(
-    #         sf_sandbox=True if sandbox == 'True' else False
-    #     )
+        res.update(
+            prepaid_physical_discon=prepaid_days,
+            postpaid_physical_discon=postpaid_days
+        )
 
-    #     return res
+        return res
 
-    # def test_credentials(self):
-    #     """
-    #     Tests the user SalesForce account credentials
-
-    #     :return:
-    #     """
-    #     _logger.info('streamtech_salesforce_connector: test_credentials')
-
-    #     try:
-    #         IrConfigParameter = self.env['ir.config_parameter'].sudo()
-    #         username = IrConfigParameter.get_param('odoo_salesforce.sf_username')
-    #         password = IrConfigParameter.get_param('odoo_salesforce.sf_password')
-    #         security = IrConfigParameter.get_param('odoo_salesforce.sf_security_token')
-    #         sandbox = IrConfigParameter.get_param('odoo_salesforce.sf_sandbox')
-    #         if sandbox:
-    #             sf = Salesforce(username=username, password=password, security_token=security, domain="test")
-    #         else:
-    #             sf = Salesforce(username=username, password=password, security_token=security)
-    #         # query = "select id,name,title,email,phone,city,country,postalCode, state, street from User"
-    #         # extend_query = " where email='" + self.env.user.email + "'"
-    #         # user = sf.query(query + extend_query)["records"][0]
-    #         # user = sf.query(query + extend_query)["records"][0]
-    #         # odoo_user = self.env['res.users'].search([('email', '=', user['Email'])])
-    #         # if odoo_user:
-    #         #     odoo_user.write({
-    #         #         'salesforce_id': user['Id'],
-    #         #     })
-    #         #     self.env.cr.commit()
-
-    #         # if not odoo_user:
-    #         #     if user['Title']:
-    #         #         user_title = self.env['res.partner.title'].search([('name', '=', user['Title'])])
-    #         #         if not user_title:
-    #         #             user_title = self.env['res.partner.title'].create({
-    #         #                 'name': user['Title']
-    #         #             })
-    #         #     user = self.env['res.users'].create({
-    #         #         'salesforce_id': user['Id'],
-    #         #         'name': user['Name'] if user['Name'] else None,
-    #         #         'login': user['Email'] if user['Email'] else None,
-    #         #         'phone': user['Phone'] if user['Phone'] else None,
-    #         #         'email': user['Email'] if user['Email'] else None,
-    #         #         'city': user['City'] if user['City'] else None,
-    #         #         'street': user['Street'] if user['Street'] else None,
-    #         #         'zip': user['PostalCode'] if user['PostalCode'] else None,
-    #         #         'state_id': self.env['res.country.state'].search(
-    #         #             [('name', '=', user['State'])]).id if
-    #         #         user['State'] else None,
-    #         #         'title': user_title.id if user_title else None,
-    #         #     })
-
-
-    #     except Exception as e:
-    #         raise Warning(_(str(e)))
-
-    #     raise osv.except_osv(_("Success!"), (_("Credentials are successfully tested.")))
