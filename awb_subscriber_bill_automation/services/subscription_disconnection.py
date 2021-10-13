b2f68c9f7c35178536fddb4ac68a6cf13c1941a3
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-10-13 18:32:55 +0800

    testing

--- awb_subscriber_bill_automation/services/subscription_disconnection.py
+++ awb_subscriber_bill_automation/services/subscription_disconnection.py
@@ -34,7 +34,7 @@ class SubscriptionDisconnect(models.Model):
         # search for the subscription using the customer_number
         for contact in contacts_to_discon:
             latest_subs = self.env['sale.subscription'].search([('customer_number', '=', contact.customer_number)], order='id desc', limit=1)
-            self._change_status_subtype(latest_subs,'disconnection-permanent')
+            self._change_status_subtype(latest_subs,'disconnection-permanent', is_closed_subs = True)
             _logger.info('FOR UPDATE IN SF')
             # try:
             #     self._update_account(main_plan, latest_subs, sf_update_type, max_fail_retries)
